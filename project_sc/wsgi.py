1d46bccded28a67123553c1d85413b33ec3ef67a
Marina Borges <marina.borges.adm@gmail.com>
2020-12-12 15:30:50 -0300

    Update wsgi.py

--- project_sc/wsgi.py
+++ project_sc/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_scientia.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_sc.settings')
 
 application = get_wsgi_application()
