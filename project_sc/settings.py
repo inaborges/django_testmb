0277f1f94f566a148e2f089f514d1e62018da1a0
Marina Borges <marina.borges.adm@gmail.com>
2020-12-12 13:49:31 -0300

    Update settings.py

--- project_sc/settings.py
+++ project_sc/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
