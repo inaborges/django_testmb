a3fdce91e4c0311df7e5e8f125d743efc9cb6651
Marina Borges <marina.borges.adm@gmail.com>
2020-12-12 15:40:18 -0300

    Update settings.py

--- project_sc/settings.py
+++ project_sc/settings.py
@@ -50,7 +50,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 # export DJANGO_DEBUG=False
 
-ALLOWED_HOSTS = ['https://django-testmb.herokuapp.com/']
+ALLOWED_HOSTS = ['django-testmb.herokuapp.com']
 
 
 # Application definition
