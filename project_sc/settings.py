243c3d9f9db82ed8062b7cbd50856b892c2f0e5b
Marina Borges <marina.borges.adm@gmail.com>
2020-12-12 14:51:20 -0300

    Update settings.py

--- project_sc/settings.py
+++ project_sc/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-@override_settings(STATIC_ROOT = {os.path.join(BASE_DIR, 'staticfiles', 'project', 'site_media', 'static-does-not-exist')})
+@override_settings(STATIC_ROOT = {os.path.join(BASE_DIR, 'staticfiles')})
+# @override_settings(DATABASES = {'default':{'ENGINE': 'django.db.backends.sqlite3'}})
 def test_empty_directory(self):
     self.run_collectstatic()
     
