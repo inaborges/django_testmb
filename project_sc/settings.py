8bad95e240a0d7aeace1bb1d97594bf0f2fd0aa9
Marina Borges <marina.borges.adm@gmail.com>
2020-12-12 15:50:47 -0300

    Update settings.py

--- project_sc/settings.py
+++ project_sc/settings.py
@@ -46,7 +46,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 # SECURITY WARNING: don't run with debug turned on in production! 
 # DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'True'
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 # export DJANGO_DEBUG=False
 
