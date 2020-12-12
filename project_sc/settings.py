0fbfc148c3378f992076b1d7262fa92283827498
Marina Borges <marina.borges.adm@gmail.com>
2020-12-12 14:40:34 -0300

    Update settings.py

--- project_sc/settings.py
+++ project_sc/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 @override_settings(STATIC_ROOT=os.path.join(BASE_DIR, 'project', 'site_media', 'static-does-not-exist'))
 def test_empty_directory(self):
