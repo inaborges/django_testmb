68092cdb8d757e748758aec5ebd86cbfd1ddbe3e
Marina Borges <marina.borges.adm@gmail.com>
2020-12-12 14:18:31 -0300

    Update settings.py

--- project_sc/settings.py
+++ project_sc/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'project_scientia.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
     }
 }
 
