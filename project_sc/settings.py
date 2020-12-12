3598cd061dc1690009d4e88e253e22794037d114
Marina Borges <marina.borges.adm@gmail.com>
2020-12-12 16:24:13 -0300

    Update settings.py

--- project_sc/settings.py
+++ project_sc/settings.py
@@ -47,12 +47,12 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production! 
 # DEBUG = True
 # DEBUG = os.environ.get('DEBUG', default=False, cast=bool)
-DEBUG = os.environ.get('DJANGO_DEBUG', 'True') != False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != False
 # DEBUG = os.getenv('DEBUG', False) == 'True'
 
 # export DJANGO_DEBUG=False
 
-ALLOWED_HOSTS = ['django-testmb.herokuapp.com']
+ALLOWED_HOSTS = ['https://django-testmb.herokuapp.com/']
 
 
 # Application definition
